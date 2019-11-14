ec357d01c7ee7f01dbf303a1d49f76207327170c
Aswini1515 <polavarapu.aswini3@gmail.com>
2019-11-14 11:18:07 +0530

    Complete Todo Project

---
+++ src/components/TodoForm.js
@@ -0,0 +1,46 @@
+import * as React from 'react';
+
+class TodoForm extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+                    newTitle: '',
+                };
+        this.handleUserInput = this.handleUserInput.bind(this);
+        this.writeTodo = this.writeTodo.bind(this);
+
+    }
+    handleUserInput(e) {
+            this.setState({
+
+                newTitle: e.target.value
+            })
+        }
+
+    writeTodo(e) {
+            var key = e.keyCode || e.which;
+            if (key === 13 && this.state.newTitle !== " ") {
+                this.props.addTodo(this.state.newTitle)
+                this.setState({
+                    newTitle: '',
+                })
+            }
+
+        }
+
+
+
+render(){
+    return(
+                    <header className="header">
+                    <h1>Todos</h1>
+                    <input className='new-todo'
+                           placeholder="What needs to be done? "
+                           value={this.state.newTitle}
+                           onChange={this.handleUserInput}
+                           onKeyPress={this.writeTodo}/>
+                    </header>
+)
+}
+}
+export default TodoForm;
